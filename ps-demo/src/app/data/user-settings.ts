2461671eaf43985939895c28f68c1e84efda3582
Alex Fuller <fulleralex47@gmail.com>
2021-03-29 07:09:20 -0400

    Insert one way data binding with a UserSettings interface

---
+++ ps-demo/src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSettings {
+  name: string,
+  subscribeEmail: boolean,
+  interfaceStyle: string,
+  subscriptionType: string,
+  notes: string
+}
--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  //One way data binding to the interface
+  userSettings: UserSettings = {
+    name: "Alex",
+    subscribeEmail: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: "notes notes notes..."
+  };
 
   constructor() { }
 
